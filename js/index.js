e1822de9527c84a8c4c33fddf23f57b7599fbe6d
BigKing <641648019@qq.com>
2020-02-14 12:07:07 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,180 @@
+﻿var arr = "2020-01-28 20:29:00".split(/[- :]/);//改这里
+var box = document.getElementsByClassName('textCon')[0];
+function init() {
+    var width = box.offsetWidth;
+    var count = parseInt(width / 50 * 5);
+    for (var i = 0; i < count; i++) {
+        var size = parseInt(ran(60, 120) / 10);
+        var ele = document.createElement('div');
+        ele.classList.add('item');
+        ele.style.width = size + 'px';
+        ele.style.height = size + 'px';
+        ele.style.left = ran(0, 95) + '%';
+        ele.style.top = ran(20, 80) + '%';
+        ele.style.animationDelay = ran(0, 30) / 10 + 's';
+        box.appendChild(ele);
+    }
+}
+function ran(min, max) {
+    min = parseInt(min);
+    max = parseInt(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function timer_start(){
+
+    var start_time = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
+    // var start_time =  new Date(beginTime);
+    // var start_time = Date.parse(beginTime);
+    var duration = parseInt(new Date()- start_time)/1000;
+    var seconds = parseInt( duration % 60);
+    if (seconds < 10)
+        seconds = "0" + seconds;
+    duration = parseInt(duration / 60);
+    var minutes = duration % 60;
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    duration = parseInt(duration / 60);
+    var hours = duration % 24;
+    if (hours < 10)
+        hours = "0" + hours;
+    duration = parseInt(duration / 24);
+    var days = duration;
+    document.getElementsByClassName('days-number')[0].innerHTML = ("" +days);
+    document.getElementsByClassName('seconds')[0].innerHTML = (hours + " 时 " + minutes + " 分 " + seconds + " 秒");
+}
+window.setInterval(timer_start,1000);
+timer_start();
+init();
+
+
+(function($) {
+
+    $.alerts = {
+        alert: function(title, message, callback) {
+            if( title == null ) title = 'Alert';
+            $.alerts._show(title, message, null, 'alert', function(result) {
+                if( callback ) callback(result);
+            });
+        },
+
+        confirm: function(title, message, callback) {
+            if( title == null ) title = 'Confirm';
+            $.alerts._show(title, message, null, 'confirm', function(result) {
+                if( callback ) callback(result);
+            });
+        },
+
+
+        _show: function(title, msg, value, type, callback) {
+
+            var _html = "";
+
+            _html += '<div id="mb_box"></div><div id="mb_con"><span id="mb_tit">' + title + '</span>';
+            _html += '<div id="mb_msg">' + msg + '</div><div id="mb_btnbox">';
+            if (type == "alert") {
+                _html += '<input id="mb_btn_ok" type="button" value="确定" />';
+            }
+            if (type == "confirm") {
+                _html += '<input id="mb_btn_ok" type="button" value="确定" />';
+                _html += '<input id="mb_btn_no" type="button" value="取消" />';
+            }
+            _html += '</div></div>';
+
+            //必须先将_html添加到body，再设置Css样式
+            $("body").append(_html); GenerateCss();
+
+            switch( type ) {
+                case 'alert':
+
+                    $("#mb_btn_ok").click( function() {
+                        $.alerts._hide();
+                        callback(true);
+                    });
+                    $("#mb_btn_ok").focus().keypress( function(e) {
+                        if( e.keyCode == 13 || e.keyCode == 27 ) $("#mb_btn_ok").trigger('click');
+                    });
+                    break;
+                case 'confirm':
+
+                    $("#mb_btn_ok").click( function() {
+                        $.alerts._hide();
+                        if( callback ) callback(true);
+                    });
+                    $("#mb_btn_no").click( function() {
+                        $.alerts._hide();
+                        if( callback ) callback(false);
+                    });
+                    $("#mb_btn_no").focus();
+                    $("#mb_btn_ok, #mb_btn_no").keypress( function(e) {
+                        if( e.keyCode == 13 ) $("#mb_btn_ok").trigger('click');
+                        if( e.keyCode == 27 ) $("#mb_btn_no").trigger('click');
+                    });
+                    break;
+
+
+            }
+        },
+        _hide: function() {
+            $("#mb_box,#mb_con").remove();
+        }
+    }
+    // Shortuct functions
+    zdalert = function(title, message, callback) {
+        $.alerts.alert(title, message, callback);
+    }
+
+    zdconfirm = function(title, message, callback) {
+        $.alerts.confirm(title, message, callback);
+    };
+
+
+
+
+    //生成Css
+    var GenerateCss = function () {
+
+        $("#mb_box").css({ width: '100%', height: '100%', zIndex: '99999', position: 'fixed',
+            filter: 'Alpha(opacity=60)', backgroundColor: 'black', top: '0', left: '0', opacity: '0.6'
+        });
+
+        $("#mb_con").css({ zIndex: '999999', width: '50%', position: 'fixed',
+            backgroundColor: 'White', borderRadius: '15px'
+        });
+
+        $("#mb_tit").css({ display: 'block', fontSize: '14px', color: '#444', padding: '10px 15px',
+            backgroundColor: '#DDD', borderRadius: '15px 15px 0 0',
+            borderBottom: '3px solid #009BFE', fontWeight: 'bold'
+        });
+
+        $("#mb_msg").css({ padding: '20px', lineHeight: '20px',
+            borderBottom: '1px dashed #DDD', fontSize: '13px'
+        });
+
+        $("#mb_ico").css({ display: 'block', position: 'absolute', right: '10px', top: '9px',
+            border: '1px solid Gray', width: '18px', height: '18px', textAlign: 'center',
+            lineHeight: '16px', cursor: 'pointer', borderRadius: '12px', fontFamily: '微软雅黑'
+        });
+
+        $("#mb_btnbox").css({ margin: '15px 0 10px 0', textAlign: 'center' });
+        $("#mb_btn_ok,#mb_btn_no").css({ width: '85px', height: '30px', color: 'white', border: 'none' });
+        $("#mb_btn_ok").css({ backgroundColor: '#168bbb' });
+        $("#mb_btn_no").css({ backgroundColor: 'gray', marginLeft: '20px' });
+
+
+        //右上角关闭按钮hover样式
+        $("#mb_ico").hover(function () {
+            $(this).css({ backgroundColor: 'Red', color: 'White' });
+        }, function () {
+            $(this).css({ backgroundColor: '#DDD', color: 'black' });
+        });
+
+        var _widht = document.documentElement.clientWidth; //屏幕宽
+        var _height = document.documentElement.clientHeight; //屏幕高
+
+        var boxWidth = $("#mb_con").width();
+        var boxHeight = $("#mb_con").height();
+
+        //让提示框居中
+        $("#mb_con").css({ top: (_height - boxHeight) / 2 + "px", left: (_widht - boxWidth) / 2 + "px" });
+    }
+})(jQuery);
